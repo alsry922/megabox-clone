8ce5a8b92ae21f50ed818740c5609de0968d903c
jinsuSang <twtz0309@naver.com>
2020-09-28 22:41:16 +0900

    setting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT;
-const publicDirectoryPath = path.join(__dirname, "../public");
+const publicPath = path.join(__dirname, "../public");
 
 const app = express();
 
-app.use(express.static(publicDirectoryPath));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.status(200).send();
+});
 
 app.listen(PORT, () => {
   console.log("Server is up to start " + PORT);
